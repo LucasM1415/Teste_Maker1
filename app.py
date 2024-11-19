bc99c48d5ffc7a4f8e58c062b875c4e510741528
LucasM1415 <luvasmanoel14@gmail.com>
2024-11-19 20:45:19 -0300

    Login e dashboard

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from routes.app_routes import app_bp
 from routes.admin_routes import admin_bp
 from routes.user_routes import user_bp   
+from routes.address_routes import address_bp  
+from routes.institution_routes import institution_bp  
 
 
 app = Flask(__name__)
@@ -18,6 +20,11 @@ app.register_blueprint(admin_bp)
 #Rotas para Usuário
 app.register_blueprint(user_bp)
 
+#Rotas para Endereço
+app.register_blueprint(address_bp)
+
+#Rotas para Instituição
+app.register_blueprint(institution_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
